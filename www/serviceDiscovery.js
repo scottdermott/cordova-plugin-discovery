1aeba4282939a37d5475fc4763d4d426e2ec9b41
Scott Dermott <scott.dermott@nagra.com>
2016-10-11 13:11:48 +0100

    removed get xml option - Now handled in Native code

--- www/serviceDiscovery.js
+++ www/serviceDiscovery.js
@@ -1,28 +1,9 @@
 /*global cordova, module*/
- function httpGet(url) {
-    var xmlHttp = null;
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET",url,false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-}
-function getConfig(devices, callback){ 
-    for (var i = 0; i < devices.length; i++) {
-        devices[i].config = httpGet(devices[i].LOCATION);
-        devices[i].url = "http://"+devices[i].LOCATION.split("/")[2];
-    }
-    callback(devices);
-}
 module.exports = {
     getNetworkServices: function (service, addConfigXML, successCallback, errorCallback) {
     	var processResponse = function (data) {
-    		if(addConfigXML) {
-    			getConfig(data, function(devices){
-    				successCallback(devices);
-    			});
-	    	} else {
-	    		successCallback(data);
-	    	}
+	    	successCallback(data);
+	    }
     	};
         cordova.exec(processResponse, errorCallback, "serviceDiscovery", "getNetworkServices", [service]);
     }
