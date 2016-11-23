7d2c0afc8d47ebc05ef0be3d812905adc9682577
Scott Dermott <scott.dermott@nagra.com>
2016-11-23 20:20:15 +0000

    * Fix merge error
    Extra closure left from merge

--- www/serviceDiscovery.js
+++ www/serviceDiscovery.js
@@ -3,7 +3,6 @@ module.exports = {
     getNetworkServices: function (service, addConfigXML, successCallback, errorCallback) {
     	var processResponse = function (data) {
 	    	successCallback(data);
-	    }
     	};
         cordova.exec(processResponse, errorCallback, "serviceDiscovery", "getNetworkServices", [service]);
     }
